b0305a346dfe64c64558b792fb75802941ba84a8
Patrick Gauvin <gauvin.patrick@gmail.com>
2017-04-02 16:28:19 -0400

    cheat splash screen

---
+++ resources/genscreen.sh
@@ -0,0 +1,30 @@
+inkscape icon.svg -w 800 -h 480 -b lightgray -y 255 -e ../www/res/screen/android/screen-hdpi-landscape.png
+inkscape icon.svg -w 480 -h 800 -b lightgray -y 255 -e ../www/res/screen/android/screen-hdpi-portrait.png
+inkscape icon.svg -w 320 -h 200 -b lightgray -y 255 -e ../www/res/screen/android/screen-ldpi-landscape.png
+inkscape icon.svg -w 200 -h 320 -b lightgray -y 255 -e ../www/res/screen/android/screen-ldpi-portrait.png
+inkscape icon.svg -w 480 -h 320 -b lightgray -y 255 -e ../www/res/screen/android/screen-mdpi-landscape.png
+inkscape icon.svg -w 320 -h 480 -b lightgray -y 255 -e ../www/res/screen/android/screen-mdpi-portrait.png
+inkscape icon.svg -w 1280 -h 720 -b lightgray -y 255 -e ../www/res/screen/android/screen-xhdpi-landscape.png
+inkscape icon.svg -w 720 -h 1280 -b lightgray -y 255 -e ../www/res/screen/android/screen-xhdpi-portrait.png
+
+inkscape icon.svg -w 480 -h 800 -b lightgray -y 255 -e ../www/res/screen/bada/screen-portrait.png
+
+inkscape icon.svg -w 320 -h 480 -b lightgray -y 255 -e ../www/res/screen/bada-wac/screen-type3.png
+inkscape icon.svg -w 480 -h 800 -b lightgray -y 255 -e ../www/res/screen/bada-wac/screen-type4.png
+inkscape icon.svg -w 240 -h 400 -b lightgray -y 255 -e ../www/res/screen/bada-wac/screen-type5.png
+
+inkscape icon.svg -w 225 -h 225 -b lightgray -y 255 -e ../www/res/screen/blackberry/screen-225.png
+
+inkscape icon.svg -w 2008 -h 1536 -b lightgray -y 255 -e ../www/res/screen/ios/screen-ipad-landscape-2x.png
+inkscape icon.svg -w 1004 -h 768 -b lightgray -y 255 -e ../www/res/screen/ios/screen-ipad-landscape.png
+inkscape icon.svg -w 1536 -h 2008 -b lightgray -y 255 -e ../www/res/screen/ios/screen-ipad-portrait-2x.png
+inkscape icon.svg -w 768 -h 1004 -b lightgray -y 255 -e ../www/res/screen/ios/screen-ipad-portrait.png
+inkscape icon.svg -w 960 -h 640 -b lightgray -y 255 -e ../www/res/screen/ios/screen-iphone-landscape-2x.png
+inkscape icon.svg -w 480 -h 320 -b lightgray -y 255 -e ../www/res/screen/ios/screen-iphone-landscape.png
+inkscape icon.svg -w 640 -h 960 -b lightgray -y 255 -e ../www/res/screen/ios/screen-iphone-portrait-2x.png
+inkscape icon.svg -w 640 -h 1136 -b lightgray -y 255 -e ../www/res/screen/ios/screen-iphone-portrait-568h-2x.png
+inkscape icon.svg -w 320 -h 480 -b lightgray -y 255 -e ../www/res/screen/ios/screen-iphone-portrait.png
+
+inkscape icon.svg -w 64 -h 64 -b lightgray -y 255 -e ../www/res/screen/webos/screen-64.png
+
+inkscape icon.svg -w 480 -h 800 -b lightgray -y 255 -e ../www/res/screen/windows-phone/screen-portrait.png
