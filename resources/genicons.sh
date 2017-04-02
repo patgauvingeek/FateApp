9321a349fe18c15e82ef1605c7c45d789a1fea7d
Patrick Gauvin <gauvin.patrick@gmail.com>
2017-04-02 15:39:13 -0400

    icon updated !

---
+++ resources/genicons.sh
@@ -0,0 +1,25 @@
+inkscape icon.svg -w 36 -h 36 -e ../www/res/icon/android/icon-36-ldpi.png
+inkscape icon.svg -w 48 -h 48 -e ../www/res/icon/android/icon-48-mdpi.png
+inkscape icon.svg -w 72 -h 72 -e ../www/res/icon/android/icon-72-hdpi.png
+inkscape icon.svg -w 96 -h 96 -e ../www/res/icon/android/icon-96-xhdpi.png
+
+inkscape icon.svg -w 128 -h 128 -e ../www/res/icon/bada/icon-128.png
+
+inkscape icon.svg -w 48 -h 48 -e ../www/res/icon/bada-wac/icon-48-type5.png
+inkscape icon.svg -w 50 -h 50 -e ../www/res/icon/bada-wac/icon-50-type3.png
+inkscape icon.svg -w 80 -h 80 -e ../www/res/icon/bada-wac/icon-80-type4.png
+
+inkscape icon.svg -w 80 -h 80 -e ../www/res/icon/blackberry/icon-80.png
+
+inkscape icon.svg -w 114 -h 114 -e ../www/res/icon/ios/icon-57-2x.png
+inkscape icon.svg -w 57 -h 57 -e ../www/res/icon/ios/icon-57.png
+inkscape icon.svg -w 144 -h 144 -e ../www/res/icon/ios/icon-72-2x.png
+inkscape icon.svg -w 72 -h 72 -e ../www/res/icon/ios/icon-72.png
+
+inkscape icon.svg -w 128 -h 128 -e ../www/res/icon/tizen/icon-128.png
+
+inkscape icon.svg -w 64 -h 64 -e ../www/res/icon/webos/icon-64.png
+
+inkscape icon.svg -w 48 -h 48 -e ../www/res/icon/windows-phone/icon-48.png
+inkscape icon.svg -w 62 -h 62 -e ../www/res/icon/windows-phone/icon-62-tile.png
+inkscape icon.svg -w 173 -h 173 -e ../www/res/icon/windows-phone/icon-173-tile.png
